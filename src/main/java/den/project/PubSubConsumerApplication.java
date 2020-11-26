4914506f254ab85b97a566eca6b63f2f02eb98ef
Denis Saenko <dsaenko@yandex.ru>
2020-11-26 22:05:59 +0100

    @GetMapping is added

--- src/main/java/den/project/PubSubConsumerApplication.java
+++ src/main/java/den/project/PubSubConsumerApplication.java
@@ -1,14 +1,29 @@
 package den.project;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.Message;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@Slf4j
 public class PubSubConsumerApplication {
+
+    @RestController
+    class HelloWorldController {
+
+        @GetMapping("/{name}")
+        String hello(@PathVariable String name) {
+            log.info("here {}" + name);
+            return "Hello " + name + "!";
+        }
+    }
 
     private MessageHandler messageHandler;
 
